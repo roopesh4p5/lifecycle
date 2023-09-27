e3aea3d9b97cefdc62684e440a2c811dc4b115f0
roopesh4p5 <108967869+roopesh4p5@users.noreply.github.com>
2023-09-27 17:30:11 +0530

    sept-27

--- lifecycle/src/app/app.component.ts
+++ lifecycle/src/app/app.component.ts
@@ -13,4 +13,52 @@ onsubmit(value:HTMLInputElement){
  this.val=value.value;
 }
 
+
+//not a life cycle hook
+constructor(){
+  console.log("0 constructor from app-----");
+  // console.log('0');
+
+}
+
+//onchange 1st
+ngOnChanges(){
+  console.log("1 ngonchanges from app------");
+  // console.log('1');
+
+}
+//oninit 2nd
+ngOnInit(){
+  console.log("2 oninit from app-----");
+  // console.log('2');
+}
+ngDoCheck(){
+  console.log('3 docheck from app------');
+  // console.log('3');
+
+}
+
+ngAfterContentInit(){
+  console.log('4 AfterContentInit from app------');
+  // console.log('4');
+}
+ngAfterContentChecked(){
+  console.log('5 Aftercontentchecked from app------');
+  // console.log('5');
+}
+ngAfterViewInit(){
+  console.log('6 After view init from app------');
+  // console.log('6');
+}
+ngAfterViewChecked(){
+  console.log('7 After view Checked from app------');
+  // console.log('7');
+}
+
+ngOnDestroy(){
+  console.log('8 After view Checked from app------');
+  // console.log('8');
+}
+
+
 }
--- lifecycle/src/app/demo/demo.component.ts
+++ lifecycle/src/app/demo/demo.component.ts
@@ -14,48 +14,48 @@ export class DemoComponent implements OnInit ,OnChanges,AfterContentInit,DoCheck
 
 //not a life cycle hook
 constructor(){
-  console.log("constructor-----");
-  console.log('0');
+  console.log("--0 constructor-----");
+  // console.log('0');
 
 }
 
 //onchange 1st
 ngOnChanges(){
-  console.log("ngonchanges------");
-  console.log('1');
+  console.log("--1 ngonchanges------");
+  // console.log('1');
 
 }
 //oninit 2nd
 ngOnInit(){
-  console.log("oninit-----");
-  console.log('2');
+  console.log("--2 oninit-----");
+  // console.log('2');
 }
 ngDoCheck(){
-  console.log('docheck------');
-  console.log('3');
+  console.log('--3 docheck------');
+  // console.log('3');
 
 }
 
 ngAfterContentInit(){
-  console.log('AfterContentInit------');
-  console.log('4');
+  console.log('--4 AfterContentInit------');
+  // console.log('4');
 }
 ngAfterContentChecked(){
-  console.log('Aftercontentchecked------');
-  console.log('5');
+  console.log('--5 Aftercontentchecked------');
+  // console.log('5');
 }
 ngAfterViewInit(){
-  console.log('After view init------');
-  console.log('6');
+  console.log('--6 After view init------');
+  // console.log('6');
 }
 ngAfterViewChecked(){
-  console.log('After view Checked------');
-  console.log('7');
+  console.log('--7 After view Checked------');
+  // console.log('7');
 }
 
 ngOnDestroy(){
-  console.log('After view Checked------');
-  console.log('8');
+  console.log('--8 After view Checked------');
+  // console.log('8');
 }
 
 }
